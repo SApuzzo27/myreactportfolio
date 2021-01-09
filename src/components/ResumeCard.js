d0a1434f68540dd49ebbaaa9090cc1cd73ff5019
Sam <SApuzzo527@gmail.com>
2021-01-09 11:46:09 -0500

    added projects js

---
+++ src/components/ResumeCard.js
@@ -0,0 +1,26 @@
+
+import React from 'react';
+import Resume from "../assets/samantha_apuzzo_resume.pdf"
+
+
+
+function ResumeCard () {
+return (
+    <section className="container-fluid resumeblock">
+        <div id="maintitle" className="row">
+        <div className="col-sm-12">
+            <h3>- my resume -</h3>
+        </div>
+        </div>
+        <div className="row">
+        <div id="aboutme" className="col-sm-12">
+            <div className="resume">
+            <embed src={Resume} width="100%" height="auto" padding-top="5px"/>
+            </div>
+        </div>
+        </div>  
+    </section>
+)
+};
+
+export default ResumeCard;
